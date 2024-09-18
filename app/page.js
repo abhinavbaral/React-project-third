3498e8ad4106101366805d3c1202cd8f0c4ac0e6
Abhinav Baral <abhinavbaral@gmail.com>
2024-09-18 15:36:55 +0545

    foreground component added and minor edit

--- app/page.js
+++ app/page.js
@@ -1,14 +1,15 @@
 "Use client"
 import React from "react";
 import Bg from "@/Components/Bg";
+import Fg from "@/Components/Fg";
 
 const page = () => {
   return (
     <>
-
       <div className=" relative w-full h-screen bg-slate-500 ">
       <Bg />
       </div>
+      <Fg />
     </>
   );
 };
