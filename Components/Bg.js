306aefad8b20fdc3ac708fe20aef1186543a9b6b
Abhinav Baral <abhinavbaral@gmail.com>
2024-09-18 15:04:00 +0545

    page edited and bg component created

---
+++ Components/Bg.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const bg = () => {
+  return (
+    <>
+       <div className="flex  justify-center text-zinc-600 py-10 text-xl font-semibold leading-none tracking-tighter">
+          Tasks list
+        </div>
+        <h1 className="absolute inset-0 flex items-center justify-center text-zinc-600 text-[10vw] font-bold leading-none tracking-tighter">
+          Tasks
+        </h1>
+    </>
+  )
+}
+
+export default bg
